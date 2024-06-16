fb2ae2dc1aa97ca923881c282a4439d5e4080fbf
olivierblanc42 <olivierhumanbooster@gmail.com>
2024-06-16 11:26:09 +0200

    ajout angular

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      card works!
+    </p>
+  `,
+  styles: ``
+})
+export class CardComponent {
+
+}
---
+++ src/app/pages/general-condition/general-condition.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general-condition',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      general-condition works!
+    </p>
+  `,
+  styles: ``
+})
+export class GeneralConditionComponent {
+
+}
